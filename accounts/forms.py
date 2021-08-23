1e025d4c2ea5550b09058888c8541daa96bb81fc
BuckytheBoss <buchman.avi@gmail.com>
2021-08-23 13:45:19 +0300

    fixed user login on signup

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,3 +1,4 @@
+from accounts.models import Profile
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -11,3 +12,9 @@ class MyUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('street_address','phone_number')
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import redirect, render
-from .forms import SignupForm, MyUserCreationForm
+from .forms import ProfileForm, SignupForm, MyUserCreationForm
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
 from .models import Profile
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -23,14 +24,31 @@ from .models import Profile
 #     return render(request, 'register.html', {'form':form})
 
 class UserCreationView(CreateView):
-    form_class = UserCreationForm
+    form_class = MyUserCreationForm
     template_name = 'register.html'
     success_url = reverse_lazy('home')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['profile_form'] = ProfileForm()
+        return context
+
     def form_valid(self, form):
-        user = form.save()
-        Profile.objects.create(user=user)
-        return super().form_valid(form)
+        profile_form = ProfileForm(self.request.POST)
+        if profile_form.is_valid():
+            new_user = form.save()
+            print(form.cleaned_data)
+            profile = profile_form.save(commit=False)
+            profile.user = new_user
+            profile.save()
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
+            print(user)
+            if user:
+                login(self.request, user)
+                print('logged in')
+            return redirect('home')
+        else:
+            return self.form_invalid(form)
 
 class MyLoginView(LoginView):
     def get(self, request, *args, **kwargs):
