fed4cc4f3c050f5a1d3b19ca9de336a7755e9934
BuckytheBoss <buchman.avi@gmail.com>
2021-08-24 12:31:58 +0300

    added a receiver

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 
@@ -10,3 +12,10 @@ class Profile(models.Model):
     
     def __str__(self):
         return f'Profile for user: {self.user.username}'
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    print('hello world')
+    if created:
+        Profile.objects.create(user=instance)
+
